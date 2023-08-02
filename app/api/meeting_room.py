e6624087a2f84762a6d502ba50ce7db64b130014
Yuriy <info@juriys.ru>
2023-08-02 18:47:58 +0300

    'add check unique name and session'

--- app/api/meeting_room.py
+++ app/api/meeting_room.py
@@ -7,9 +7,9 @@
 create_meeting_room() и поэтому сама тоже должна быть асинхронной:
 в ней тоже нужно применить ключевые слова async и await.
 """
-from app.crud.meeting_room import create_meeting_room
+from app.crud.meeting_room import create_meeting_room, get_room_id_by_name
 from app.schemas.meeting_room import MeetingRoomCreate
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 router = APIRouter()
 
@@ -30,5 +30,13 @@ async def create_new_meeting_room(
 
     Роутер подключим его к объекту приложения.
     """
+    # Вызываем функцию проверки уникальности поля name:
+    room_id = await get_room_id_by_name(meeting_room.name)
+    # Если такой объект уже есть в базе - вызываем ошибку:
+    if room_id is not None:
+        raise HTTPException(
+            status_code=422,
+            detail='Переговорка с таким именем уже существует!',
+        )
     new_room = await create_meeting_room(meeting_room)
     return new_room
--- app/crud/meeting_room.py
+++ app/crud/meeting_room.py
@@ -8,10 +8,14 @@
 Чтобы передать полученные в запросе данные из
 Pydantic-схемы в ORM-модель — потребуется конвертировать схему в словарь.
 """
-# Импортируем sessionmaker из файла с настройками БД.
+from typing import Optional
+# Добавляем импорт функции select.
+from sqlalchemy import select
+
 from app.core.db import AsyncSessionLocal
 from app.models.meeting_room import MeetingRoom
 from app.schemas.meeting_room import MeetingRoomCreate
+
 
 
 # Функция работает с асинхронной сессией,
@@ -54,3 +58,28 @@ async def create_meeting_room(
         await session.refresh(db_room)
     # Возвращаем только что созданный объект класса MeetingRoom.
     return db_room
+
+
+# Добавляем новую асинхронную функцию.
+async def get_room_id_by_name(room_name: str) -> Optional[int]:
+    """
+    Проверяем уникальность имени переговорки.
+
+    Добавьте в файл app/crud/meeting_room.py асинхронную
+    функцию get_room_id_by_name(), которая примет на вход имя переговорки
+    (поле name из полученного запроса).
+    Если в базе будет найден объект с таким же name — функция вернёт id
+    этого объекта; возвращать весь объект целиком нет смысла, ведь
+    цель — просто узнать, есть ли в базе такой объект.
+    Если в базе нет одноимённой переговорки — функция вернёт None.
+    """
+    async with AsyncSessionLocal() as session:
+        # Получаем объект класса Result.
+        db_room_id = await session.execute(
+            select(MeetingRoom.id).where(
+                MeetingRoom.name == room_name
+            )
+        )
+        # Извлекаем из него конкретное значение.
+        db_room_id = db_room_id.scalars().first()
+    return db_room_id
