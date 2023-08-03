21bbc0bbc58554e21cc38afe20842b694b485e43
Yuriy <info@juriys.ru>
2023-08-03 16:15:27 +0300

    create dependens session

--- app/api/meeting_room.py
+++ app/api/meeting_room.py
@@ -7,9 +7,15 @@
 create_meeting_room() и поэтому сама тоже должна быть асинхронной:
 в ней тоже нужно применить ключевые слова async и await.
 """
+# Импортируем асинхронный генератор сессий.
+from app.core.db import get_async_session
 from app.crud.meeting_room import create_meeting_room, get_room_id_by_name
-from app.schemas.meeting_room import MeetingRoomCreate
-from fastapi import APIRouter, HTTPException
+from app.schemas.meeting_room import MeetingRoomCreate, MeetingRoomDB
+from fastapi import APIRouter, Depends, HTTPException
+
+# Импортируем класс асинхронной сессии для аннотации параметра.
+from sqlalchemy.ext.asyncio import AsyncSession
+
 
 router = APIRouter()
 
@@ -21,22 +27,38 @@ router = APIRouter()
 # в таком случае нужно ставить закрывающий слеш.
 
 
-@router.post('/meeting_rooms/')
+@router.post(
+    '/meeting_rooms/',
+    # Указываем схему ответа.
+    response_model=MeetingRoomDB,
+    # Не выводим пустые поля и дефолт
+    response_model_exclude_none=True,
+    # Исключать значение по умолчанию
+    # response_model_exclude_defaults=True,
+)
 async def create_new_meeting_room(
-    meeting_room: MeetingRoomCreate,
+        meeting_room: MeetingRoomCreate,
+        # Указываем зависимость, предоставляющую объект сессии,
+        # как параметр функции.
+        session: AsyncSession = Depends(get_async_session),
 ):
     """
     Создаем новую комнату.
 
     Роутер подключим его к объекту приложения.
     """
+
+    # Вторым параметром передаём сессию в CRUD-функцию:
     # Вызываем функцию проверки уникальности поля name:
-    room_id = await get_room_id_by_name(meeting_room.name)
+
+    room_id = await get_room_id_by_name(meeting_room.name, session)
     # Если такой объект уже есть в базе - вызываем ошибку:
+
     if room_id is not None:
         raise HTTPException(
             status_code=422,
             detail='Переговорка с таким именем уже существует!',
         )
-    new_room = await create_meeting_room(meeting_room)
+    # Вторым параметром передаём сессию в CRUD-функцию:
+    new_room = await create_meeting_room(meeting_room, session)
     return new_room
--- app/schemas/meeting_room.py
+++ app/schemas/meeting_room.py
@@ -4,11 +4,34 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 
-class MeetingRoomCreate(BaseModel):
+# Базовый класс схемы, от которого наследуем все остальные.
+class MeetingRoomBase(BaseModel):
+    """Создадим базовый класс модели комнаты."""
+
+    name: Optional[str] = Field(None, min_length=1, max_length=100)
+    description: Optional[str]
+
+
+class MeetingRoomCreate(MeetingRoomBase):
     """Модель переговорки, для Post запроса, название обязательно."""
 
     name: str = Field(
         ..., min_length=1, max_length=100,
         title='Название переговорки', description='Можно в любом регистре'
     )
-    description: Optional[str]
+
+
+class MeetingRoomDB(MeetingRoomCreate):
+    """Возвращаем ответ после создания комнаты."""
+
+    id: int
+
+    class Config:
+        """
+        Чтобы FastAPI мог сериализовать объект ORM-модели.
+
+        В схему MeetingRoomDB, нужно указать, что схема может принимать
+        на вход объект базы данных, а не только Python-словарь или JSON-объект.
+        """
+
+        orm_mode = True
